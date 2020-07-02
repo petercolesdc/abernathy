5ede532a0ddbcd748578e96e2d71a5a173484668
Pete Coles <pete@fffunction.co>
2020-07-02 12:48:06 +0100

    JS

--- js/global.js
+++ js/global.js
@@ -3,6 +3,8 @@
 document.documentElement.className = 'js';
 
 // Check to see if touchscreen and append body accordingly
+// Note: This is not to be relied upon, it is intended as a
+// general check for prototyping
 if(window.matchMedia("(pointer: coarse)").matches) {
   document.querySelector('html').classList.add('touch');
 } else {
