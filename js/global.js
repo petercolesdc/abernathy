e104912481742a8d0b6821db3a4f0ca021dfa7bf
Pete Coles <pete@fffunction.co>
2020-09-07 00:48:10 +0100

    removed color on buttons. a11y improvements

--- js/global.js
+++ js/global.js
@@ -10,3 +10,12 @@ if(window.matchMedia("(pointer: coarse)").matches) {
 } else {
   document.querySelector('html').classList.add('no-touch');
 }
+
+// Listen to tab events to enable outlines (accessibility improvement)
+function handleFirstTab(e) {
+  if (e.keyCode === 9) { // the "I am a keyboard user" key
+      document.body.classList.add('user-is-tabbing');
+      window.removeEventListener('keydown', handleFirstTab);
+  }
+}
+window.addEventListener('keydown', handleFirstTab);
